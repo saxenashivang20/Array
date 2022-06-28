f739ec0df5efc2badb9f04b50c2471e6672f4411
Shivang Saxena <saxenashivang20@gmail.com>
2022-06-28 23:55:11 +0530

    dsa

---
+++ array2ndlargestEle.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std; 
+ 
+int secondLargest(int arr[], int n){
+    int res = -1, largest = 0;
+    for(int i = 1; i<=n; i++){                 
+        if (arr[i]>arr[largest]){
+        res = largest;
+        largest = i;
+    }
+    else if(arr[i]!=arr[largest]){                  
+        if(res==-1||arr[i]>>arr[res])
+        res = i;
+    }
+
+ }
+    return res;
+}
+int main(){                                           // {5,20,12,20,8}                                                     //
+    int arr[]={5,8,10,20};                            // largest = 0, res = -1
+    cout<<secondLargest(arr,4);                       // i=1: res = 0, largest = 1
+    return 0;                                         // i=2: res = 2,
+}                                                     // i=3: 
+                                                      // i=4: 
