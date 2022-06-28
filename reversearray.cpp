f3484a9a465e25e174e4777a326699c4167d9020
Shivang Saxena <saxenashivang20@gmail.com>
2022-06-29 00:09:08 +0530

    dsa

--- reversearray.cpp
+++ reversearray.cpp
@@ -5,21 +5,15 @@ using namespace std;
  void reverse(int arr[], int n)
 {
 	int low = 0, high = n - 1;
-
 	while(low < high)
 	{
-		int temp = arr[low];
-
+		int temp = arr[low];    // swap arr[low] and arr[high]
 		arr[low] = arr[high];
-
 		arr[high] = temp;
-
 		low++;
 		high--;
 	}
 }
-
-    
 int main() {
 	
       int arr[] = {10, 5, 7, 30}, n = 4;
