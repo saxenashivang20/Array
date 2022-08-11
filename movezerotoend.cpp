e6e11e2b97adde7c168e69b55b689540ed75e60a
Shivang Saxena <91368870+saxenashivang20@users.noreply.github.com>
2022-08-11 23:51:55 +0530

    Update movezerotoend.cpp

--- movezerotoend.cpp
+++ movezerotoend.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-void movezeroes(int arr[], int n){
+void movezeroes(int arr[], int n){  // main function 
     int count= 0;
     for(int i =0; i<n;i++){
         if(arr[]!=0){
